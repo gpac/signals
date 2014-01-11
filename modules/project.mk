6dac395ae6cc0b9e6efe36479a71dcdec849d87c
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-01-11 10:54:25 +0100

    Add 'modules' project to Makefile
    Former-commit-id: 4b4123eb9f2fb1cc917cfb615e013caabdb24a4b

--- modules/project.mk
+++ modules/project.mk
@@ -1,6 +1,7 @@
 MODULES_SRCS:=\
   $(ROOT)/internal/log.cpp\
   $(ROOT)/src/demux/gpac_mp4_simple.cpp\
+  $(ROOT)/src/demux/gpac_mp4_full.cpp\
   $(ROOT)/src/in/file.cpp\
   $(ROOT)/src/out/print.cpp\
 
