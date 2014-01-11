5b57b19059b793cd5925418320890e5ae81008ef
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-01-11 11:31:58 +0100

    Fix commit error

--- modules/src/demux/gpac_mp4_simple.cpp
+++ modules/src/demux/gpac_mp4_simple.cpp
@@ -53,7 +53,7 @@ void GPAC_MP4_Simple::deleteLastSample() {
 
 GPAC_MP4_Simple::GPAC_MP4_Simple(GF_ISOFile *movie)
 : reader(new ISOFileReader) {
-	gf_sys_init(0);
+	gf_sys_init(GF_FALSE);
 	u32 track_id = gf_isom_get_track_id(movie, 1); //FIXME should be a parameter? hence not processed in create() but in a stateful process? or a control module?
 	reader->track_number = gf_isom_get_track_by_id(movie, track_id);
 	if (reader->track_number == 0) {
