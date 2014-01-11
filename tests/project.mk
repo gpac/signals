8f3d365d21225dedd712d7e64a652201a52f9474
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-01-11 11:50:14 +0100

    Add missing project file
    Former-commit-id: 7a3ea39fb3841e0fa0d1e990ed7f257973a696b6

---
+++ tests/project.mk
@@ -0,0 +1,25 @@
+TARGETS+=$(BIN)/$(ProjectName)/signals_simple.exe
+$(BIN)/$(ProjectName)/signals_simple.exe: $(BIN)/$(ProjectName)/signals_simple.o
+DEPS+=$(BIN)/$(ProjectName)/signals_simple.deps
+
+TARGETS+=$(BIN)/$(ProjectName)/modules_demux.exe
+MODULES_DEMUX_OBJS:=$(BIN)/$(ProjectName)/modules_demux.o $(MODULES_OBJS)
+$(BIN)/$(ProjectName)/modules_demux.exe: $(MODULES_DEMUX_OBJS)
+DEPS+=$(MODULES_DEMUX_OBJS:%.o=.deps)
+
+TARGETS+=$(BIN)/$(ProjectName)/signals_perf.exe
+$(BIN)/$(ProjectName)/signals_perf.exe: $(BIN)/$(ProjectName)/signals_perf.o
+DEPS+=$(BIN)/$(ProjectName)/signals_perf.deps
+
+TARGETS+=$(BIN)/$(ProjectName)/signals_module.exe
+$(BIN)/$(ProjectName)/signals_module.exe: $(BIN)/$(ProjectName)/signals_module.o
+DEPS+=$(BIN)/$(ProjectName)/signals_module.deps
+
+TARGETS+=$(BIN)/$(ProjectName)/signals_async.exe
+$(BIN)/$(ProjectName)/signals_async.exe: $(BIN)/$(ProjectName)/signals_async.o
+DEPS+=$(BIN)/$(ProjectName)/signals_async.deps
+
+TARGETS+=$(BIN)/$(ProjectName)/signals_unit_result.exe
+$(BIN)/$(ProjectName)/signals_unit_result.exe: $(BIN)/$(ProjectName)/signals_unit_result.o
+DEPS+=$(BIN)/$(ProjectName)/signals_unit_result.deps
+
