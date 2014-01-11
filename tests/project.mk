4c3952047a4040178c10477f47aff530cd7d2bb2
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-01-11 15:59:22 +0100

    Makefile: factor variable
    Former-commit-id: 7554883537c649a21c6ae7a4dc01c1f8882d4571

--- tests/project.mk
+++ tests/project.mk
@@ -1,33 +1,36 @@
-TARGETS+=$(BIN)/$(ProjectName)/signals_simple.exe
-$(BIN)/$(ProjectName)/signals_simple.exe: $(BIN)/$(ProjectName)/signals_simple.o
-DEPS+=$(BIN)/$(ProjectName)/signals_simple.deps
 
-TARGETS+=$(BIN)/$(ProjectName)/modules_demux.exe
-MODULES_DEMUX_OBJS:=$(BIN)/$(ProjectName)/modules_demux.o $(MODULES_OBJS)
-$(BIN)/$(ProjectName)/modules_demux.exe: $(MODULES_DEMUX_OBJS)
+OUTDIR:=$(BIN)/$(ProjectName)
+
+TARGETS+=$(OUTDIR)/signals_simple.exe
+$(OUTDIR)/signals_simple.exe: $(OUTDIR)/signals_simple.o
+DEPS+=$(OUTDIR)/signals_simple.deps
+
+TARGETS+=$(OUTDIR)/modules_demux.exe
+MODULES_DEMUX_OBJS:=$(OUTDIR)/modules_demux.o $(MODULES_OBJS)
+$(OUTDIR)/modules_demux.exe: $(MODULES_DEMUX_OBJS)
 DEPS+=$(MODULES_DEMUX_OBJS:%.o=%.deps)
 
-TARGETS+=$(BIN)/$(ProjectName)/signals_perf.exe
-$(BIN)/$(ProjectName)/signals_perf.exe: $(BIN)/$(ProjectName)/signals_perf.o
-DEPS+=$(BIN)/$(ProjectName)/signals_perf.deps
+TARGETS+=$(OUTDIR)/signals_perf.exe
+$(OUTDIR)/signals_perf.exe: $(OUTDIR)/signals_perf.o
+DEPS+=$(OUTDIR)/signals_perf.deps
 
-TARGETS+=$(BIN)/$(ProjectName)/signals_module.exe
-$(BIN)/$(ProjectName)/signals_module.exe: $(BIN)/$(ProjectName)/signals_module.o
-DEPS+=$(BIN)/$(ProjectName)/signals_module.deps
+TARGETS+=$(OUTDIR)/signals_module.exe
+$(OUTDIR)/signals_module.exe: $(OUTDIR)/signals_module.o
+DEPS+=$(OUTDIR)/signals_module.deps
 
-TARGETS+=$(BIN)/$(ProjectName)/signals_async.exe
-$(BIN)/$(ProjectName)/signals_async.exe: $(BIN)/$(ProjectName)/signals_async.o
-DEPS+=$(BIN)/$(ProjectName)/signals_async.deps
+TARGETS+=$(OUTDIR)/signals_async.exe
+$(OUTDIR)/signals_async.exe: $(OUTDIR)/signals_async.o
+DEPS+=$(OUTDIR)/signals_async.deps
 
-TARGETS+=$(BIN)/$(ProjectName)/signals_unit_result.exe
-$(BIN)/$(ProjectName)/signals_unit_result.exe: $(BIN)/$(ProjectName)/signals_unit_result.o
-DEPS+=$(BIN)/$(ProjectName)/signals_unit_result.deps
+TARGETS+=$(OUTDIR)/signals_unit_result.exe
+$(OUTDIR)/signals_unit_result.exe: $(OUTDIR)/signals_unit_result.o
+DEPS+=$(OUTDIR)/signals_unit_result.deps
 
 run: unit
-	PROGRAM=$(realpath $(BIN)/$(ProjectName)/modules_demux.exe) && cd $(ProjectName) && $$PROGRAM
-	$(BIN)/$(ProjectName)/signals_unit_result.exe
-	$(BIN)/$(ProjectName)/signals_simple.exe
-	$(BIN)/$(ProjectName)/signals_perf.exe
-	$(BIN)/$(ProjectName)/signals_module.exe
-	$(BIN)/$(ProjectName)/signals_async.exe
+	PROGRAM=$(realpath $(OUTDIR)/modules_demux.exe) && cd $(ProjectName) && $$PROGRAM
+	$(OUTDIR)/signals_unit_result.exe
+	$(OUTDIR)/signals_simple.exe
+	$(OUTDIR)/signals_perf.exe
+	$(OUTDIR)/signals_module.exe
+	$(OUTDIR)/signals_async.exe
 
