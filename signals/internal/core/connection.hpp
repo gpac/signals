528e12fd3d1f100d890e10db258fb505a13b5cb9
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-01-14 23:03:02 +0100

    Factor specialization for void
    Former-commit-id: cffaa52c238c0a09be58bb378ef6409ea1194ffa

--- signals/internal/core/connection.hpp
+++ signals/internal/core/connection.hpp
@@ -4,50 +4,40 @@
 #include <future>
 #include "../utils/queue.hpp"
 
+template<typename T>
+struct NotVoid
+{
+	typedef T Type;
+};
+
+template<>
+struct NotVoid<void>
+{
+	typedef int Type;
+};
+
 
 template<typename Callback, typename ResultType>
 class ConnectionQueue {
 public:
+	typedef typename NotVoid<ResultType>::Type FutureResultType;
 	Callback callback;
-	Queue<std::shared_future<ResultType>> futures;
+	Queue<std::shared_future<FutureResultType>> futures;
 	size_t uid;
 
 	explicit ConnectionQueue(const Callback &callback, const size_t uid) : callback(callback), uid(uid) {
 	}
 };
-
-//specialized for void
-template<typename Callback>
-class ConnectionQueue<Callback, void> {
-public:
-	Callback callback;
-	Queue<std::shared_future<void>> futures;
-	size_t uid;
-
-	explicit ConnectionQueue(const Callback &callback, const size_t uid) : callback(callback), uid(uid) {
-	}
-};
-
 
 template<typename Callback, typename ResultType>
 class ConnectionQueueThreadSafe {
 public:
+	typedef typename NotVoid<ResultType>::Type FutureResultType;
 	Callback callback;
-	QueueThreadSafe<std::shared_future<ResultType>> futures;
+	QueueThreadSafe<std::shared_future<FutureResultType>> futures;
 	size_t uid;
 
 	explicit ConnectionQueueThreadSafe(const Callback &callback, const size_t uid) : callback(callback), uid(uid) {
 	}
 };
 
-//specialized for void
-template<typename Callback>
-class ConnectionQueueThreadSafe<Callback, void> {
-public:
-	Callback callback;
-	QueueThreadSafe<std::shared_future<int>> futures;
-	size_t uid;
-
-	explicit ConnectionQueueThreadSafe(const Callback &callback, const size_t uid) : callback(callback), uid(uid) {
-	}
-};
