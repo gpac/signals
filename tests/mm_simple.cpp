09f33c4edcf4c23cc148e5cacddb3f74909731d2
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2014-01-18 12:26:35 +0100

    formatting

--- tests/mm_simple.cpp
+++ tests/mm_simple.cpp
@@ -9,11 +9,8 @@ using namespace MM;
 unittest("Pull2Push sub module: print packets size from file: File -> Out::Print") {
 	//FIXME: this is a custom version of File
 	MM::File *f = MM::File::create("data/BatmanHD_1000kbit_mpeg.mp4");
-
 	Pull2Push *p2p = Pull2Push::create();
-
 	std::unique_ptr<MM::Module> m(MM::Module::create(p2p, f));
-
 	std::unique_ptr<Out::Print> p(Out::Print::create(std::cout));
 
 	CONNECT(m.get(), signals[0]->signal, p.get(), &Out::Print::process);
