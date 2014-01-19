b97de74fa028f7fd43e24a143551338ea356a0df
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2014-01-19 13:25:17 +0100

    set FIXME back
    Former-commit-id: c35e5b84d6a775b6fd0a34a33f9e6c5489459305

--- tests/modules_erasure.cpp
+++ tests/modules_erasure.cpp
@@ -2,7 +2,7 @@
 #include "modules.hpp"
 #include <memory>
 
-#include "libavcodec/avcodec.h"
+#include "libavcodec/avcodec.h" //FIXME: there should be none of the modules include at the application level
 
 using namespace Tests;
 using namespace Modules;
