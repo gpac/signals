f8dced48e74f0775bdf48e519f702eac7325e6fb
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-01-19 11:54:11 +0100

    libav modules: don't call register everytime a module is created, only at program startup. Reduce local variable scopes.
    Former-commit-id: cb6a0957d527463ce02e89de0839165b3708ad8f

---
+++ utils/tools.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+// Runs a function at instanciation:
+// Use to initialize C libraries at program startup.
+// Example:
+// auto g_InitAv = runAtStartup(&av_register_all);
+struct DummyStruct {
+};
+
+template<class R, class... Args>
+DummyStruct runAtStartup(R f(Args...), Args... argVal)
+{
+	f(argVal...);
+	return DummyStruct();
+}
