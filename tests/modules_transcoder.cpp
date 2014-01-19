bd41ac01e9fe37c2a1d6f8ec73bf5ae7367fe07a
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2014-01-19 19:26:35 +0100

    improve audio + add a (currently failing) test
    Former-commit-id: 8820de91ebf4056d05bad233dcd5b9f165b6c817

--- tests/modules_transcoder.cpp
+++ tests/modules_transcoder.cpp
@@ -62,4 +62,57 @@ namespace {
 		encode->destroy();
 	}
 
+	unittest("transcoder: audio simple") {
+		//create demux
+		std::unique_ptr<Demux::LibavDemux> demux(Demux::LibavDemux::create("data/BatmanHD_1000kbit_mpeg_0_20_frag_1000.mp4"));
+		ASSERT(demux != nullptr);
+
+		//create stub output (for unused demuxer's outputs)
+		std::unique_ptr<Out::Null> null(Out::Null::create());
+		ASSERT(null != nullptr);
+
+		//find video signal from demux
+		size_t videoIndex = std::numeric_limits<size_t>::max();
+		for (size_t i = 0; i < demux->signals.size(); ++i) {
+			Props *props = demux->signals[i]->props.get();
+			PropsDecoder *decoderProps = dynamic_cast<PropsDecoder*>(props);
+			ASSERT(decoderProps);
+			if (decoderProps->getAVCodecContext()->codec_type == AVMEDIA_TYPE_AUDIO) { //TODO: expose it somewhere
+				videoIndex = i;
+			} else {
+				//FIXME: we have to set Print output to avoid asserts. Should be remove once the framework is more tested.
+				Connect(demux->signals[i]->signal, null.get(), &Out::Null::process);
+			}
+		}
+		ASSERT(videoIndex != std::numeric_limits<size_t>::max());
+
+		//create the video decoder
+		Props *props = demux->signals[videoIndex]->props.get();
+		PropsDecoder *decoderProps = dynamic_cast<PropsDecoder*>(props);
+		std::unique_ptr<Decode::LibavDecode> decode(Decode::LibavDecode::create(*decoderProps));
+		ASSERT(decode != nullptr);
+
+		//create the mux
+		std::unique_ptr<Mux::LibavMux> mux(Mux::LibavMux::create("output"));
+		ASSERT(mux != nullptr);
+
+		//create the encoder
+		props = mux->signals[0]->props.get();
+		PropsMuxer *muxerProps = dynamic_cast<PropsMuxer*>(props);
+		std::unique_ptr<Encode::LibavEncode> encode(Encode::LibavEncode::create(*muxerProps, Encode::LibavEncode::Audio));
+		ASSERT(encode != nullptr);
+
+		Connect(demux.get()->signals[videoIndex]->signal, decode.get(), &Decode::LibavDecode::process);
+		Connect(decode.get()->signals[0]->signal, encode.get(), &Encode::LibavEncode::process);
+		Connect(encode.get()->signals[0]->signal, mux.get(), &Mux::LibavMux::process);
+
+		while (demux->process(nullptr)) {
+		}
+
+		demux->destroy();
+		decode->destroy();
+		mux->destroy();
+		encode->destroy();
+	}
+
 }
