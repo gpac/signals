e75994951c2360eba12c57ce0321f0704fe5e30b
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2014-01-23 10:39:07 +0100

    set pull2push and reorder on the same model

--- mm/internal/pull2push.hpp
+++ mm/internal/pull2push.hpp
@@ -1,40 +1,36 @@
 #pragma once
 
-#include "submodule.hpp"
-#include <atomic>
+#include <../modules/modules.hpp>
 
 namespace MM {
 
-class Pull2Push : public Submodule {
+//TODO: we should be able to run this module easily in a separate thread
+class Pull2Push : public Modules::Module {
 public:
+	Pull2Push(Modules::Module *module) : delegate(module) {
+		signals.push_back(new Pin); //TODO: this super module should copy the structure from the delegate, see Reorder
+		Connect(getSignal(0), delegate.get(), &Modules::Module::process); //delegate output to this output ; faster is delegate output signal is sync
+	}
 	~Pull2Push() {
 		delete signals[0];
 	}
-
-	static Pull2Push* create() {
-		return new Pull2Push();
-	}
-
 	bool process(std::shared_ptr<Data> data) {
 		for (;;) {
 			std::shared_ptr<Data> out(signals[0]->getBuffer(0));
 			signals[0]->emit(out);
-			auto res = getSignal(0).results(false, true); //this code also works with async, why not use it?
+			auto res = getSignal(0).results(false, true);
 			if (res->size() && ((*res)[0] == false)) {
 				break;
 			}
 		}
 		return true;
 	}
-
-	bool handles(const std::string &/*url*/) {
-		return true;
+	bool handles(const std::string &url) {
+		return delegate->handles(url);
 	}
 
 private:
-	Pull2Push() {
-		signals.push_back(new PinSync());
-	}
+	std::unique_ptr<Modules::Module> delegate;
 };
 
 }
