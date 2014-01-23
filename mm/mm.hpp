9fb626ec0c1c76ad91eaadee7afcd196100c6c42
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2014-01-23 10:39:07 +0100

    comment unused

--- mm/mm.hpp
+++ mm/mm.hpp
@@ -2,9 +2,9 @@
 #define _MM_HPP_
 
 #include "internal/config.hpp"
-#include "internal/module.hpp"
+//#include "internal/module.hpp"
 #include "internal/pull2push.hpp"
 #include "internal/reorder.hpp"
-#include "internal/submodule.hpp"
+//#include "internal/submodule.hpp"
 
 #endif
