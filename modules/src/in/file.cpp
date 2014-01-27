d510a9b351214ffa9a265030d6d1bbd5e64a06aa
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2014-01-27 19:04:57 +0100

    remove unused
    Former-commit-id: 54de4d27fa6e33d21eb48e826bd927280c694cf2

--- modules/src/in/file.cpp
+++ modules/src/in/file.cpp
@@ -1,8 +1,6 @@
 #include "../utils/log.hpp"
 #include "file.hpp"
 
-#include <iostream> //Romain
-
 #define IOSIZE 65536
 
 namespace Modules {
@@ -29,7 +27,6 @@ File* File::create(std::string const& fn) {
 bool File::process(std::shared_ptr<Data> /*data*/) {
 	std::shared_ptr<Data> out(signals[0]->getBuffer(IOSIZE));
 	size_t read = fread(out->data(), 1, IOSIZE, file);
-	std::cout << "File: read data of size: " << read << std::endl;
 	if (read < IOSIZE) {
 		if (read == 0) {
 			return false;
