a8cab8f5bcc8ef420d79eea45b3a93cfab550b61
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2014-01-27 21:57:40 +0100

    cosmetics

--- mm/mm.hpp
+++ mm/mm.hpp
@@ -2,9 +2,7 @@
 #define _MM_HPP_
 
 #include "internal/config.hpp"
-//#include "internal/module.hpp"
 #include "internal/pull2push.hpp"
 #include "internal/reorder.hpp"
-//#include "internal/submodule.hpp"
 
 #endif
