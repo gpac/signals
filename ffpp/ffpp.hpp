f3283c6fc916fb0f7cb0556d0b994a27b59211b6
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-02-19 07:58:34 +0100

    Wrap AVFrame into ffpp::Frame. Handles automatic freeing.

--- ffpp/ffpp.hpp
+++ ffpp/ffpp.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdexcept>
+
 extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
@@ -8,4 +10,28 @@ extern "C" {
 #include <libavutil/opt.h>
 }
 
+namespace ffpp {
+	struct Frame {
+		Frame() {
+			avFrame = avcodec_alloc_frame();
+			if(!avFrame)
+				throw std::runtime_error("Frame allocation failed");
+		}
+
+		~Frame() {
+			avcodec_free_frame(&avFrame);
+		}
+
+		AVFrame* operator->() {
+			return avFrame;
+		}
+
+		AVFrame* get() {
+			return avFrame;
+		}
+
+	private:
+		AVFrame* avFrame;
+	};
+}
 
