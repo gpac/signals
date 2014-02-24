e2daf7a246061935763b294613346e966500c2b9
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-02-25 00:18:59 +0100

    SDL audio renderer: extract (potentially reusable) ring-buffer logic to a separate class.

--- modules/src/render/sdl_audio.hpp
+++ modules/src/render/sdl_audio.hpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include <mutex>
 #include <string>
+#include <vector>
 
 struct SDL_Rect;
 struct SDL_Renderer;
@@ -12,6 +13,41 @@ struct SDL_Texture;
 
 namespace Modules {
 namespace Render {
+
+// TODO: move elsewhere
+class Fifo {
+public:
+
+	void write(const uint8_t* data, size_t len) {
+		m_data.resize(m_writePos + len);
+		memcpy(&m_data[m_writePos], data, len);
+		m_writePos += len;
+	}
+
+	const uint8_t* readPointer() {
+		return &m_data[m_readPos];
+	}
+
+	void consume(int numBytes) {
+		assert(numBytes <= bytesToRead());
+		m_readPos += numBytes;
+
+		// shift everything to the beginning of the buffer
+		auto const remaining = bytesToRead();
+		memmove(m_data.data(), m_data.data() + m_readPos, remaining);
+		m_writePos -= remaining;
+		m_readPos -= remaining;
+	}
+
+	int bytesToRead() const {
+		return m_writePos - m_readPos;
+	}
+
+private:
+	int m_writePos;
+	int m_readPos;
+	std::vector<uint8_t> m_data;
+};
 
 //FIXME: check it doesn't need to run in thread 0, like most render on Unix do because of X11...
 class MODULES_EXPORT SDLAudio : public Module {
@@ -28,9 +64,7 @@ private:
 	void fillAudio(uint8_t *stream, int len);
 
 	std::mutex m_Mutex;
-	uint32_t m_audioLen;
-	uint8_t *m_audioPos;
-	std::vector<uint8_t> m_audioData;
+	Fifo m_Fifo;
 };
 
 }
