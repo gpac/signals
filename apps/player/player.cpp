a3de7e018bbcbd9c85bc0f9958f525eeba47f16c
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-02-24 21:01:18 +0100

    Factor player code. Audio and video use the same control flow (avoid code duplication)

--- apps/player/player.cpp
+++ apps/player/player.cpp
@@ -8,60 +8,58 @@
 using namespace Tests;
 using namespace Modules;
 
+std::unique_ptr<Module> renderPin(Pin* pPin, PropsDecoder* decoderProps) {
+	auto const codec_type = decoderProps ? decoderProps->getAVCodecContext()->codec_type : AVMEDIA_TYPE_UNKNOWN;
+	if (codec_type == AVMEDIA_TYPE_VIDEO) {
+		Log::msg(Log::Info, "Found video stream");
+		auto r = uptr(Render::SDLVideo::create());
+		ConnectPin(pPin, r.get(), &Render::SDLVideo::process);
+		return std::move(r);
+	} else if (codec_type == AVMEDIA_TYPE_AUDIO) {
+		Log::msg(Log::Info, "Found audio stream");
+		auto r = uptr(Render::SDLAudio::create());
+		ConnectPin(pPin, r.get(), &Render::SDLAudio::process);
+		return std::move(r);
+	} else {
+		Log::msg(Log::Info, "Found unknown stream");
+		auto r = uptr(Out::Null::create());
+		ConnectPin(pPin, r.get(), &Out::Null::process);
+		return std::move(r);
+	}
+}
+
 int safeMain(int argc, char const* argv[]) {
 
 	if(argc != 2)
 		throw std::runtime_error("usage: player <file>");
 
 	auto const inputFile = argv[1];
-	
+
+	std::list<std::unique_ptr<Module>> modules;
+
 	{
 		auto demux = uptr(Demux::LibavDemux::create(inputFile));
-		auto null = uptr(Out::Null::create());
-
-		int videoIndex = -1;
-		int audioIndex = -1;
 
 		for (size_t i = 0; i < demux->getNumPin(); ++i) {
 			Props *props = demux->getPin(i)->getProps();
 			PropsDecoder *decoderProps = dynamic_cast<PropsDecoder*>(props);
 			ASSERT(decoderProps);
-			if (decoderProps->getAVCodecContext()->codec_type == AVMEDIA_TYPE_VIDEO) {
-				videoIndex = i;
-			} else if (decoderProps->getAVCodecContext()->codec_type == AVMEDIA_TYPE_AUDIO) {
-				audioIndex = i;
-			} else {
-				ConnectPin(demux->getPin(i), null.get(), &Out::Null::process); //FIXME: this is a stub to void the assert of not connected signals...
-			}
+
+			auto decoder = uptr(Decode::LibavDecode::create(*decoderProps));
+			ConnectPin(demux->getPin(i), decoder.get(), &Decode::LibavDecode::process);
+
+			auto renderer = renderPin(decoder->getPin(0), decoderProps);
+
+			modules.push_back(std::move(decoder));
+			modules.push_back(std::move(renderer));
 		}
-
-		if(videoIndex == -1)
-			throw std::runtime_error("Video stream not found in file");
-
-		if(audioIndex == -1)
-			throw std::runtime_error("Audio stream not found in file");
-
-		Props *videoProps = demux->getPin(videoIndex)->getProps();
-		PropsDecoder *videoDecoderProps = dynamic_cast<PropsDecoder*>(videoProps);
-		auto videoDecoder = uptr(Decode::LibavDecode::create(*videoDecoderProps));
-		auto videoRenderer = uptr(Render::SDLVideo::create());
-		ConnectPin(demux->getPin(videoIndex), videoDecoder.get(), &Decode::LibavDecode::process);
-		ConnectPin(videoDecoder->getPin(0), videoRenderer.get(), &Render::SDLVideo::process);
-
-		Props *audioProps = demux->getPin(audioIndex)->getProps();
-		PropsDecoder *audioDecoderProps = dynamic_cast<PropsDecoder*>(audioProps);
-		auto audioDecoder = uptr(Decode::LibavDecode::create(*audioDecoderProps));
-		auto audioRenderer = uptr(Render::SDLAudio::create());
-
-		ConnectPin(demux->getPin(audioIndex), audioDecoder.get(), &Decode::LibavDecode::process);
-		ConnectPin(audioDecoder->getPin(0), audioRenderer.get(), &Render::SDLAudio::process);
 
 		while (demux->process(nullptr)) {
 		}
 
-		demux->waitForCompletion();
-		videoDecoder->waitForCompletion();
-		audioDecoder->waitForCompletion();
+		foreach(i, modules) {
+			(*i)->waitForCompletion();
+		}
 	}
 
 	return 0;
