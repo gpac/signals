155517e4a3152fac6c0f62fb9aecb4966c4e7304
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-03-03 23:19:39 +0100

    Move Fifo class to its own file
    Former-commit-id: a80d62c387868b1627660b410271ea4a8b1f7339

--- modules/src/render/sdl_audio.hpp
+++ modules/src/render/sdl_audio.hpp
@@ -2,6 +2,7 @@
 
 #include "../../internal/config.hpp"
 #include "internal/module.hpp"
+#include "fifo.hpp"
 #include <memory>
 #include <mutex>
 #include <string>
@@ -14,44 +15,6 @@ struct SDL_Texture;
 
 namespace Modules {
 namespace Render {
-
-// TODO: move elsewhere
-class Fifo {
-public:
-
-	Fifo() : m_writePos(0), m_readPos(0) {
-	}
-
-	void write(const uint8_t* data, size_t len) {
-		m_data.resize(m_writePos + len);
-		memcpy(&m_data[m_writePos], data, len);
-		m_writePos += len;
-	}
-
-	const uint8_t* readPointer() {
-		return &m_data[m_readPos];
-	}
-
-	void consume(size_t numBytes) {
-		assert(numBytes >= 0);
-		assert(numBytes <= bytesToRead());
-		m_readPos += numBytes;
-
-		// shift everything to the beginning of the buffer
-		memmove(m_data.data(), m_data.data() + m_readPos, bytesToRead());
-		m_writePos -= m_readPos;
-		m_readPos = 0;
-	}
-
-	size_t bytesToRead() const {
-		return m_writePos - m_readPos;
-	}
-
-private:
-	size_t m_writePos;
-	size_t m_readPos;
-	std::vector<uint8_t> m_data;
-};
 
 //FIXME: check it doesn't need to run in thread 0, like most render on Unix do because of X11...
 class MODULES_EXPORT SDLAudio : public Module {
