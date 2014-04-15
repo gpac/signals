979e0558874c550a403391eacc098da73ef8b6e9
rbouqueau <romain.bouqueau.pro@gmail.com>
2014-04-15 22:57:16 +0200

    fix msvc build

--- tests/modules_clock.cpp
+++ tests/modules_clock.cpp
@@ -9,12 +9,12 @@ using namespace Modules;
 namespace {
 
 unittest("Basic clock") {
-	for(int i=0;i < 10;++i) {
+	for (int i = 0; i < 10; ++i) {
 		auto const now = g_DefaultClock->now();
 		std::cout << "Time: " << now << std::endl;
 
-    auto const duration = std::chrono::milliseconds(20);
-    std::this_thread::sleep_for(duration);
+		auto const duration = std::chrono::milliseconds(20);
+		std::this_thread::sleep_for(duration);
 	}
 }
 
