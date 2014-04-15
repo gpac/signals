949e4577301a53f1fba95f420d6ead24c30e93e1
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-04-15 19:41:38 +0200

    Add an audio sine generator

---
+++ modules/src/in/sound_generator.cpp
@@ -0,0 +1,46 @@
+#include "../utils/log.hpp"
+#include "../utils/tools.hpp"
+#include "sound_generator.hpp"
+#include <cmath>
+
+auto const BUFFER_SIZE = 128;
+
+namespace Modules {
+namespace In {
+
+SoundGenerator::SoundGenerator()
+	: m_numSamples(0) {
+	signals.push_back(uptr(pinFactory->createPin()));
+}
+
+bool SoundGenerator::process(std::shared_ptr<Data> /*data*/) {
+	std::shared_ptr<PcmData> out(new PcmData(BUFFER_SIZE));
+
+	// generate sound
+	auto const bytesPerSample = 2;
+  auto const p = out->data();
+	for(int i=0;i < BUFFER_SIZE/bytesPerSample;++i)
+	{
+		auto const phase = m_numSamples * 2.0 * 440.0 * M_PI / 44100.0;
+		auto const fVal = sin(phase);
+		auto const val = int(fVal * 32767.0f);
+		p[i*2+0] = (val >> 0) & 0xFF;
+		p[i*2+1] = (val >> 8) & 0xFF;
+
+		m_numSamples++;
+	}
+
+	signals[0]->emit(out);
+	return true;
+}
+
+bool SoundGenerator::handles(const std::string &url) {
+	return SoundGenerator::canHandle(url);
+}
+
+bool SoundGenerator::canHandle(const std::string &/*url*/) {
+	return true;
+}
+
+}
+}
