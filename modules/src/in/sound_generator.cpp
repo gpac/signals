5dfda30d7234262c6152bf194a7f65077fdf1e13
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-04-15 20:30:32 +0200

    Fix stereo

--- modules/src/in/sound_generator.cpp
+++ modules/src/in/sound_generator.cpp
@@ -9,7 +9,7 @@
 
 auto const BUFFER_SIZE = 128;
 auto const SAMPLE_RATE = 44100;
-auto const SINE_FREQ = 440.0;
+auto const SINE_FREQ = 880.0;
 
 namespace Modules {
 namespace In {
@@ -23,13 +23,19 @@ bool SoundGenerator::process(std::shared_ptr<Data> /*data*/) {
 	std::shared_ptr<PcmData> out(new PcmData(BUFFER_SIZE));
 
 	// generate sound
-	auto const bytesPerSample = 2;
+	auto const bytesPerSample = 4;
   auto const p = out->data();
 	for(int i=0;i < BUFFER_SIZE/bytesPerSample;++i) {
 		auto const fVal = nextSample();
 		auto const val = int(fVal * 32767.0f);
-		p[i*2+0] = (val >> 0) & 0xFF;
-		p[i*2+1] = (val >> 8) & 0xFF;
+
+		// left
+		p[i*bytesPerSample+0] = (val >> 0) & 0xFF;
+		p[i*bytesPerSample+1] = (val >> 8) & 0xFF;
+
+		// right
+		p[i*bytesPerSample+2] = (val >> 0) & 0xFF;
+		p[i*bytesPerSample+3] = (val >> 8) & 0xFF;
 	}
 
 	signals[0]->emit(out);
