0ec2237f52a335a32d54c32f2bd2b6b396e4a889
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-04-16 19:29:34 +0200

    A/V sync test: add a two-thread test to allow testing A/V sync when signals is in PinSync mode
    Former-commit-id: b90414f35f446654eda767c3fe021fd4a38c2025

--- tests/modules_render.cpp
+++ tests/modules_render.cpp
@@ -13,25 +13,55 @@ using namespace Modules;
 
 namespace {
 
-unittest("A/V sync") {
+unittest("A/V sync: one thread") {
+	auto videoGen = uptr(new In::VideoGenerator);
+	auto videoRender = uptr(Render::SDLVideo::create());
+	ConnectPin(videoGen->getPin(0), videoRender.get(), &Render::SDLVideo::process);
 
 	auto soundGen = uptr(new In::SoundGenerator);
 	auto soundRender = uptr(Render::SDLAudio::create());
 	ConnectPin(soundGen->getPin(0), soundRender.get(), &Render::SDLAudio::process);
 
-	auto videoGen = uptr(new In::VideoGenerator);
-	auto videoRender = uptr(Render::SDLVideo::create());
-	ConnectPin(videoGen->getPin(0), videoRender.get(), &Render::SDLVideo::process);
-
 	for(int i=0; i < 25*5; ++i) {
-		soundGen->process(nullptr);
 		videoGen->process(nullptr);
+		soundGen->process(nullptr);
 	}
 
 	videoGen->waitForCompletion();
-	soundGen->waitForCompletion();
 	videoRender->waitForCompletion();
+	soundGen->waitForCompletion();
 	soundRender->waitForCompletion();
+}
+
+unittest("A/V sync: separate threads") {
+	auto f = [&]() {
+		auto videoGen = uptr(new In::VideoGenerator);
+		auto videoRender = uptr(Render::SDLVideo::create());
+		ConnectPin(videoGen->getPin(0), videoRender.get(), &Render::SDLVideo::process);
+
+		for(int i=0; i < 25*5; ++i) {
+			videoGen->process(nullptr);
+		}
+
+		videoGen->waitForCompletion();
+		videoRender->waitForCompletion();
+	};
+	auto g = [&]() {
+		auto soundGen = uptr(new In::SoundGenerator);
+		auto soundRender = uptr(Render::SDLAudio::create());
+		ConnectPin(soundGen->getPin(0), soundRender.get(), &Render::SDLAudio::process);
+		for(int i=0; i < 25*5; ++i) {
+			soundGen->process(nullptr);
+		}
+
+		soundGen->waitForCompletion();
+		soundRender->waitForCompletion();
+	};
+
+	std::thread tf(f);
+	std::thread tg(g);
+	tf.join();
+	tg.join();
 }
 
 }
