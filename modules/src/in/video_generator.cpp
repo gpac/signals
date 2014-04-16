c21b337a6a599c6d0c2d84330ef76f3d694b6365
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-04-16 11:55:35 +0200

    video generator: generate only 2 frames per flash period. Add test
    Former-commit-id: 0454d371e067b360bfb54bd76b65038cd46b9d9d

--- modules/src/in/video_generator.cpp
+++ modules/src/in/video_generator.cpp
@@ -17,16 +17,18 @@ bool VideoGenerator::process(std::shared_ptr<Data> /*data*/) {
 	auto const picSize = VIDEO_WIDTH * VIDEO_HEIGHT * 3 / 2;
 	std::shared_ptr<PcmData> out(new PcmData(picSize));
 
-	auto const framePeriodIn180k = 180000LL / FRAMERATE;
-	out->setTime(m_numFrames * framePeriodIn180k);
-
 	// generate video
 	auto const p = out->data();
 	auto const FLASH_PERIOD = FRAMERATE;
 	auto const flash = (m_numFrames % FLASH_PERIOD) == 0;
 	auto const val = flash ? 0xCC : 0x80;
 	memset(p, val, picSize);
-	signals[0]->emit(out);
+
+	auto const framePeriodIn180k = 180000LL / FRAMERATE;
+	out->setTime(m_numFrames * framePeriodIn180k);
+
+	if(m_numFrames % 25 < 2)
+		signals[0]->emit(out);
 
 	++m_numFrames;
 	return true;
