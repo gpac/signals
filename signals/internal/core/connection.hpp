6689afde9afe10c50de463b0fe956606f2169b54
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-05-12 21:15:10 +0200

    Add const

--- signals/internal/core/connection.hpp
+++ signals/internal/core/connection.hpp
@@ -10,9 +10,9 @@ template<typename Callback, typename ResultType>
 class ConnectionList { //TODO: write interface
 public:
 	typedef NotVoid<ResultType> FutureResultType;
-	Callback callback;
+	Callback const callback;
+	size_t const uid;
 	std::vector<std::shared_future<FutureResultType>> futures;
-	size_t uid;
 
 	explicit ConnectionList(const Callback &callback, const size_t uid) : callback(callback), uid(uid) {
 	}
