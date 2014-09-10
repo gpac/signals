7aadb0a3df558e6da44c1b13e1df7e17db8c6291
rbouqueau <romain.bouqueau.pro@gmail.com>
2014-09-10 10:08:41 +0200

    add first version of dashcastx (would need the latest improvements from apps/player)

---
+++ apps/dashcastx/project.mk
@@ -0,0 +1,9 @@
+OUTDIR:=$(BIN)/$(ProjectName)
+
+TARGETS+=$(OUTDIR)/dashcastx.exe
+EXE_DASHCASTX_OBJS:=\
+	$(LIB_MODULES_OBJS)\
+	$(UTILS_OBJS)\
+ 	$(OUTDIR)/dashcastx.o
+$(OUTDIR)/dashcastx.exe:  $(EXE_DASHCASTX_OBJS)
+DEPS+=$(EXE_DASHASTX_OBJS:%.o=%.deps)
