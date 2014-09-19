c504747a3415f904c7eafcd29d300120f1d4de5e
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-09-19 23:33:06 +0200

    Don't disable Ctrl+C when initting SDL
    Former-commit-id: 31685947b9057ac531ad875fc2cd122f326b5a3e

--- modules/src/render/sdl_common.cpp
+++ modules/src/render/sdl_common.cpp
@@ -1,9 +1,17 @@
 #include "../utils/log.hpp"
 #include "SDL2/SDL.h"
 
+#ifdef __linux__
+#include <signal.h>
+#endif
+
 class SdlInit {
 public:
 	SdlInit() {
+#ifdef __linux__
+		struct sigaction action;
+		sigaction(SIGINT, nullptr, &action);
+#endif
 		if (SDL_InitSubSystem(SDL_INIT_AUDIO | SDL_INIT_NOPARACHUTE) == -1) {
 			Log::msg(Log::Warning, "[SDLAudio render] Couldn't initialize: %s", SDL_GetError());
 			throw std::runtime_error("Init failed");
@@ -12,6 +20,9 @@ public:
 			Log::msg(Log::Warning, "[SDLVideo render] Couldn't initialize: %s", SDL_GetError());
 			throw std::runtime_error("Init failed");
 		}
+#ifdef __linux__
+		sigaction(SIGINT, &action, nullptr);
+#endif
 	}
 
 	~SdlInit() {
