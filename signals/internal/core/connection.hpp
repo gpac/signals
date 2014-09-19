2decf801c6eab4a4d3dc7c5d9589a2f1b19acb7f
rbouqueau <romain.bouqueau.pro@gmail.com>
2014-09-19 19:22:22 +0200

    fix build following a too-much merging

--- signals/internal/core/connection.hpp
+++ signals/internal/core/connection.hpp
@@ -11,11 +11,11 @@ class ConnectionList { //TODO: write interface
 public:
 	typedef NotVoid<ResultType> FutureResultType;
 	ICaller<ResultType(Args...)> &caller;
-	std::function<Callback(Args...)> const callback;
+	std::function<ResultType(Args...)> const callback;
 	size_t const uid;
 	std::vector<std::shared_future<FutureResultType>> futures;
 
-	explicit ConnectionList(ICaller<ResultType(Args...)> &caller, const std::function<Callback(Args...)> &callback, const size_t uid) : caller(caller), callback(callback), uid(uid) {
+	explicit ConnectionList(ICaller<ResultType(Args...)> &caller, const std::function<ResultType(Args...)> &callback, const size_t uid) : caller(caller), callback(callback), uid(uid) {
 	}
 };
 
