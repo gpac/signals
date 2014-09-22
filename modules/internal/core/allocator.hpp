16cdeb45e231aa0c97a139fd76adb7b2509897e4
Samurai Akihiro <samurai.akihiro@gmail.com>
2014-09-22 23:32:31 +0200

    Remove COUNT_ALLOC. Put a name on lambda.

--- modules/internal/core/allocator.hpp
+++ modules/internal/core/allocator.hpp
@@ -2,7 +2,6 @@
 
 #include "data.hpp"
 #include <algorithm>
-#include <atomic>
 #include <list>
 #include <memory>
 
@@ -18,9 +17,6 @@ class AllocatorPacket {
 public:
 	AllocatorPacket(size_t numBlocks = 10)
 		: numBlocks(numBlocks)
-#ifdef COUNT_ALLOC
-		, numAlloc(0)
-#endif
 	{
 	}
 
@@ -29,9 +25,6 @@ public:
 		if (usedBlocks.size() < numBlocks) {
 			std::shared_ptr<DataType> data(new DataType(size));
 			usedBlocks.push_back(std::weak_ptr<DataType>(data));
-#ifdef COUNT_ALLOC
-			numAlloc++;
-#endif
 			return data;
 		}
 
@@ -39,9 +32,6 @@ public:
 			numBlocks++;
 			std::shared_ptr<DataType> data(new DataType(size));
 			usedBlocks.push_back(std::weak_ptr<DataType>(data));
-#ifdef COUNT_ALLOC
-			numAlloc++;
-#endif
 			return data;
 		}
 		
@@ -57,12 +47,6 @@ public:
 		return usedBlocks.size();
 	}
 
-#ifdef COUNT_ALLOC
-	uint64_t getNumAlloc() const {
-		return numAlloc;
-	}
-#endif
-
 	void reset() {
 		for (auto &block : usedBlocks) {
 			block.reset();
@@ -75,15 +59,13 @@ private:
 
 	size_t numBlocks;
 	std::list<std::weak_ptr<Data>> usedBlocks;
-#ifdef COUNT_ALLOC
-	std::atomic<uint64_t> numAlloc;
-#endif
 
 	void updateUsedBlocks() {
-		usedBlocks.erase(std::remove_if(usedBlocks.begin(), usedBlocks.end(),
-		[](std::weak_ptr<Data> data) -> bool {
+		auto isFree = [](std::weak_ptr<Data> data) {
 			return data.expired();
-		}), usedBlocks.end());
+		};
+			
+		usedBlocks.erase(std::remove_if(usedBlocks.begin(), usedBlocks.end(), isFree), usedBlocks.end());
 	}
 
 };
