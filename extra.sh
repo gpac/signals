10797f6baaca0700f1b3fd9b0ac4d280724b5c91
rbouqueau <romain.bouqueau.pro@gmail.com>
2014-09-24 21:38:34 +0200

    update the gpac deps to build statically

--- extra.sh
+++ extra.sh
@@ -87,7 +87,7 @@ if [ ! -f extra/src/gpac/Changelog ] ;
 then
 	mkdir -p extra/src
 	rm -rf extra/src/gpac
-	svn checkout svn://svn.code.sf.net/p/gpac/code/trunk/gpac -r 5426 extra/src/gpac
+	svn checkout svn://svn.code.sf.net/p/gpac/code/trunk/gpac -r 5442 extra/src/gpac
 fi
 
 if [ ! -f extra/build/gpac/buildOk ] ;
