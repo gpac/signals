79e6bf8be687d1581aee1a047eca63916e3aa482
rbouqueau <romain.bouqueau.pro@gmail.com>
2014-09-27 11:30:56 +0200

    improvement to test the gpac mux: testing a mux is not easy...

--- tests/modules_stub.cpp
+++ tests/modules_stub.cpp
@@ -2,6 +2,7 @@
 
 #include "../modules/src/demux/gpac_demux_mp4_simple.hpp"
 #include "../modules/src/mux/gpac_mux_mp4.hpp"
+#include "../modules/src/out/null.hpp"
 #include "../modules/internal/utils/helper.hpp"
 
 #include "tools.hpp"
@@ -14,32 +15,34 @@ namespace {
 
 unittest("stub: gpac mp4 remux: GPAC demux -> GPAC mux") {
 	auto demux = uptr(Demux::GPACDemuxMP4Simple::create("data/BatmanHD_1000kbit_mpeg_0_20_frag_1000.mp4"));
+	auto null = uptr(Out::Null::create());
 
 	//find video signal from demux
 	size_t videoIndex = std::numeric_limits<size_t>::max();
-#if 0
 	for (size_t i = 0; i < demux->getNumPin(); ++i) {
 		auto props = demux->getPin(i)->getProps();
+#if 0
 		PropsDecoder *decoderProps = dynamic_cast<PropsDecoder*>(props);
 		ASSERT(decoderProps);
 		if (decoderProps->getAVCodecContext()->codec_type == AVMEDIA_TYPE_VIDEO) { //TODO: expose it somewhere
+#else
+		if (i == 0) {
+#endif
 			videoIndex = i;
-		}
-		else {
+		} else {
 			ConnectPinToModule(demux->getPin(i), null); //FIXME: this is a stub to void the assert of not connected signals...
 		}
 	}
-#else
-	videoIndex = 0;
-#endif
 	ASSERT(videoIndex != std::numeric_limits<size_t>::max());
 
-	//auto props = demux->getPin(videoIndex)->getProps();
+	auto props = demux->getPin(videoIndex)->getProps();
 	auto mux = uptr(new Mux::GPACMuxMP4("output_video_gpac"));
 
 	//pass meta data between encoder and mux
-	//Connect(demux->declareStream, mux.get(), &Mux::GPACMuxMP4::declareStream);
-	//demux->sendOutputPinsInfo();
+#if 0
+	Connect(demux->declareStream, mux.get(), &Mux::GPACMuxMP4::declareStream);
+	demux->sendOutputPinsInfo();
+#endif
 
 	ConnectPinToModule(demux->getPin(videoIndex), mux);
 
