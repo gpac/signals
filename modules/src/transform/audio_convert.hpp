3cf7a7224078e217caa8f7d2c10d5fc9f3c311d5
rbouqueau <romain.bouqueau.pro@gmail.com>
2014-11-11 09:18:34 +0100

    remove the avFrame from the audio converter and abide to libav idiosyncrasies

--- modules/src/transform/audio_convert.hpp
+++ modules/src/transform/audio_convert.hpp
@@ -24,7 +24,6 @@ class AudioConvert : public Module {
 
 	private:
 		std::unique_ptr<PcmFormat> srcPcmFormat, dstPcmFormat;
-		std::unique_ptr<ffpp::Frame> const aFrame;
 		std::unique_ptr<ffpp::SwResampler> const m_Swr;
 };
 
