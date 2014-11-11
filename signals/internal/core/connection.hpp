5c3c7d2927734f3dce5b1a4ed5c3a377be6c4d1b
rbouqueau <romain.bouqueau.pro@gmail.com>
2014-11-11 18:55:28 +0100

    specialize ConnectionList for void return type - avoid shitty shared_future microsoft c++ runtime behaviour

--- signals/internal/core/connection.hpp
+++ signals/internal/core/connection.hpp
@@ -7,7 +7,7 @@
 namespace Signals {
 
 template<typename ResultType, typename... Args>
-class ConnectionList { //TODO: write interface
+class ConnectionList {
 public:
 	typedef NotVoid<ResultType> FutureResultType;
 	IExecutor<ResultType(Args...)> &executor;
@@ -16,6 +16,28 @@ public:
 	std::vector<std::shared_future<FutureResultType>> futures;
 
 	explicit ConnectionList(IExecutor<ResultType(Args...)> &executor, const std::function<ResultType(Args...)> &callback, const size_t uid) : executor(executor), callback(callback), uid(uid) {
+	}
+};
+
+template<typename... Args>
+class ConnectionList<void, Args...> {
+private:
+	template<typename T>
+	class FakeVector {
+	public:
+		void push_back(T const&) { }
+		T* erase(T*) { return nullptr; }
+		T* begin() { return nullptr; }
+		T* end() { return nullptr; }
+	};
+
+public:
+	IExecutor<void(Args...)> &executor;
+	std::function<void(Args...)> const callback;
+	size_t const uid;
+	FakeVector<std::shared_future<NotVoid<void>>> futures;
+
+	explicit ConnectionList(IExecutor<void(Args...)> &executor, const std::function<void(Args...)> &callback, const size_t uid) : executor(executor), callback(callback), uid(uid) {
 	}
 };
 
