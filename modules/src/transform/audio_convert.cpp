ac00b026f081bf6beb407b85ea105b107a2ca4e1
rbouqueau <romain.bouqueau.pro@gmail.com>
2014-11-11 08:24:39 +0100

    rename
    Former-commit-id: d38e237b2fe92e4db9697ecba3b58c12b42d6cec

--- modules/src/transform/audio_convert.cpp
+++ modules/src/transform/audio_convert.cpp
@@ -10,15 +10,15 @@ namespace Transform {
 
 AudioConvert::AudioConvert(AudioSampleFormat srcFmt, AudioLayout srcChannelLayout, uint32_t srcSampleRate, AudioStruct srcStruct,
 	AudioSampleFormat dstFmt, AudioLayout dstChannelLayout, uint32_t dstSampleRate, AudioStruct dstStruct)
-	: srcAudioCfg(new PcmFormat(srcSampleRate, srcChannelLayout, srcFmt, srcStruct)),
-	  dstAudioCfg(new PcmFormat(dstSampleRate, dstChannelLayout, dstFmt, dstStruct)),
+	: srcPcmFormat(new PcmFormat(srcSampleRate, srcChannelLayout, srcFmt, srcStruct)),
+	  dstPcmFormat(new PcmFormat(dstSampleRate, dstChannelLayout, dstFmt, dstStruct)),
 	  aFrame(new ffpp::Frame), m_Swr(new ffpp::SwResampler) {
 	AVSampleFormat avDstFmt;
 	uint64_t avDstChannelLayout;
 	int avDstNumChannels, avDstSampleRate;
 
-	libavAudioCtxConvertLibav(srcAudioCfg.get(), aFrame->get()->sample_rate, aFrame->get()->format, aFrame->get()->channels, aFrame->get()->channel_layout);
-	libavAudioCtxConvertLibav(dstAudioCfg.get(), avDstSampleRate, (int&)avDstFmt, avDstNumChannels, avDstChannelLayout);
+	libavAudioCtxConvertLibav(srcPcmFormat.get(), aFrame->get()->sample_rate, aFrame->get()->format, aFrame->get()->channels, aFrame->get()->channel_layout);
+	libavAudioCtxConvertLibav(dstPcmFormat.get(), avDstSampleRate, (int&)avDstFmt, avDstNumChannels, avDstChannelLayout);
 	
 	m_Swr->setInputSampleFmt((AVSampleFormat)aFrame->get()->format);
 	m_Swr->setInputLayout(aFrame->get()->channel_layout);
@@ -37,12 +37,12 @@ AudioConvert::~AudioConvert() {
 
 void AudioConvert::process(std::shared_ptr<Data> data) {
 	auto audioData = safe_cast<PcmData>(data);
-	if (audioData->getFormat() != *srcAudioCfg)
+	if (audioData->getFormat() != *srcPcmFormat)
 		throw std::runtime_error("[AudioConvert] Incompatible input audio data");
 	libavFrameDataConvert(audioData.get(), aFrame->get());
 
 	auto const srcNumSamples = aFrame->get()->nb_samples;
-	auto const dstNumSamples = divUp(srcNumSamples * dstAudioCfg->sampleRate, srcAudioCfg->sampleRate);
+	auto const dstNumSamples = divUp(srcNumSamples * dstPcmFormat->sampleRate, srcPcmFormat->sampleRate);
 
 	const int bufferSize = av_samples_get_buffer_size(nullptr, aFrame->get()->channels, aFrame->get()->nb_samples, (AVSampleFormat)aFrame->get()->format, 0);
 	auto out(pins[0]->getBuffer(bufferSize * 10)); //Romain: pourquoi *10?
@@ -50,8 +50,8 @@ void AudioConvert::process(std::shared_ptr<Data> data) {
 
 	/*auto const numSamples = */m_Swr->convert(&pDst, dstNumSamples, (const uint8_t**)aFrame->get()->data, srcNumSamples);
 	assert(m_Swr->getDelay(IClock::Rate) == 0);
-	//auto const sampleSize = numSamples * dstAudioCfg->getBytesPerSample();
-	//assert(data->getTime() == sampleSize * IClock::Rate / dstAudioCfg->sampleRate);
+	//auto const sampleSize = numSamples * dstPcmFormat->getBytesPerSample();
+	//assert(data->getTime() == sampleSize * IClock::Rate / dstPcmFormat->sampleRate);
 	
 	out->setTime(data->getTime());
 	pins[0]->emit(out);
