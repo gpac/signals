9a4c7a452e6c5cce95717b0d6c127d53195bbe7f
rbouqueau <romain.bouqueau.pro@gmail.com>
2014-11-15 19:41:28 +0100

    update the stranded executor after tests
    Former-commit-id: 2c2c314abbbb7f99572cb189f7fbd6162a392a9e

--- modules/internal/utils/stranded_pool_executor.cpp
+++ modules/internal/utils/stranded_pool_executor.cpp
@@ -9,10 +9,10 @@ namespace Modules {
 #define N_THREADS_PER_CPU 1 //TODO: run perf tests - some modules may be blocking somehow? (I/O, own parallelisme pattern as in x264, etc.)
 static asio::thread_pool g_threadPool { N_THREADS_PER_CPU * std::thread::hardware_concurrency() };
 
-StrandedPoolModuleExecutor::StrandedPoolModuleExecutor() : threadPool(g_threadPool), strand(threadPool.get_executor()) {
+StrandedPoolModuleExecutor::StrandedPoolModuleExecutor() : strand(g_threadPool.get_executor()) {
 }
 
-StrandedPoolModuleExecutor::StrandedPoolModuleExecutor(asio::thread_pool &threadPool) : threadPool(threadPool), strand(threadPool.get_executor()) {
+StrandedPoolModuleExecutor::StrandedPoolModuleExecutor(asio::thread_pool &threadPool) : strand(threadPool.get_executor()) {
 }
 
 StrandedPoolModuleExecutor::~StrandedPoolModuleExecutor() {
@@ -20,11 +20,11 @@ StrandedPoolModuleExecutor::~StrandedPoolModuleExecutor() {
 
 std::shared_future<NotVoid<void>> StrandedPoolModuleExecutor::operator() (const std::function<void(std::shared_ptr<Data>)> &fn, std::shared_ptr<Data> data) {
 	std::shared_future<NotVoid<void>> future = std::async(std::launch::deferred, [] { return NotVoid<void>(); });
-	auto closure = [future, &fn, data]() -> void {
+	auto closure = [future, fn, data]() -> void {
 		fn(data);
 		future.get();
 	};
-	asio::dispatch(strand, closure);
+	asio::post(strand, closure);
 	return future;
 }
 
