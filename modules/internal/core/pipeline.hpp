2084b9a321181b9e52c7dde7c3a4196762f04487
Samurai Akihiro <samurai.akihiro@gmail.com>
2015-02-09 19:25:33 +0100

    Move pipeline to its own file
    Former-commit-id: 2ce1cb48478e5aaccd2d5a0439f7816222cab090

---
+++ modules/internal/core/pipeline.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include "internal/core/data.hpp"
+#include "internal/utils/stranded_pool_executor.hpp"
+
+namespace Modules {
+
+class Module;
+class IPin;
+
+#define EXECUTOR_SYNC ExecutorSync<void(std::shared_ptr<const Data>)>
+#define EXECUTOR_ASYNC StrandedPoolModuleExecutor
+#define EXECUTOR EXECUTOR_ASYNC
+
+struct ICompletionNotifier {
+	virtual void finished() = 0;
+};
+
+class PipelinedModule {
+public:
+	/* take ownership of module */
+	PipelinedModule(Module *module, ICompletionNotifier *notify);
+	void connect(IPin* pin);
+	size_t getNumPin() const;
+	IPin* getPin(int i) const;
+
+	/* direct call: receiving nullptr stops the execution */
+	void process(std::shared_ptr<const Data> data);
+
+	/* same as process() but uses the executor (may defer the call) */
+	void dispatch(std::shared_ptr<const Data> data);
+
+	/* source modules are stopped manually - then the message propagates to other connected modules */
+	void setSource(bool isSource);
+	bool isSource() const;
+	bool isSink() const;
+
+private:
+	void endOfStream();
+
+	enum Type {
+		None,
+		Source
+	};
+	Type type;
+
+	std::unique_ptr<Module> delegate;
+	std::unique_ptr<IProcessExecutor> const localExecutor;
+	IProcessExecutor &executor;
+	ICompletionNotifier* const m_notify;
+};
+
+class Pipeline : public ICompletionNotifier {
+public:
+	Pipeline();
+	PipelinedModule* addModule(Module* rawModule, bool isSource = false);
+	void connect(IPin* pin, PipelinedModule *module);
+	void start();
+	void waitForCompletion();
+	void finished() override;
+
+private:
+	std::vector<std::unique_ptr<PipelinedModule>> modules;
+
+	std::mutex mutex;
+	std::condition_variable condition;
+	std::atomic<int> numRemainingNotifications;
+};
+
+}
