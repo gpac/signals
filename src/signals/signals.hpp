70cf07b7fd0dc48a15f82ab988de432761471067
Samurai Akihiro <samurai.akihiro@gmail.com>
2015-02-10 16:43:47 +0100

    Use pragma once
    Former-commit-id: 03684448d7bf738d0e17b54fd07598cc39e7805c

--- src/signals/signals.hpp
+++ src/signals/signals.hpp
@@ -1,8 +1,6 @@
-#ifndef _SIGNALS_HPP_
-#define _SIGNALS_HPP_
+#pragma once
 
 #include "internal/utils/helper.hpp"
 #include "internal/utils/threadpool.hpp"
 #include "internal/core/signal.hpp"
 
-#endif
