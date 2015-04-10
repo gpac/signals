6f276c8b0edddff4b83d3faaeb947221a91972e1
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2015-04-10 09:34:01 +0200

    add dtsc header + cosmetics
    Former-commit-id: 6ef98286307c696e2703218ea406ce9de6f39cce

--- src/lib_modules/core/system_clock.cpp
+++ src/lib_modules/core/system_clock.cpp
@@ -7,17 +7,17 @@ namespace Modules {
 using namespace std::chrono;
 
 class SystemClock : public IClock {
-	public:
-		SystemClock() : m_Start(high_resolution_clock::now()) {
-		}
-		virtual uint64_t now() const {
-			auto const timeNow = high_resolution_clock::now();
-			auto const timeNowInMs = duration_cast<milliseconds>(timeNow - m_Start);
-			assert(IClock::Rate % 1000 == 0);
-			return timeNowInMs.count() * (IClock::Rate / 1000LL);
-		}
-	private:
-		time_point<high_resolution_clock> const m_Start;
+public:
+	SystemClock() : m_Start(high_resolution_clock::now()) {
+	}
+	virtual uint64_t now() const {
+		auto const timeNow = high_resolution_clock::now();
+		auto const timeNowInMs = duration_cast<milliseconds>(timeNow - m_Start);
+		assert(IClock::Rate % 1000 == 0);
+		return timeNowInMs.count() * (IClock::Rate / 1000LL);
+	}
+private:
+	time_point<high_resolution_clock> const m_Start;
 };
 
 IClock* createSystemClock() {
