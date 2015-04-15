bfcf5a7a778243544808cdc256e93dda48c2b034
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2015-04-15 13:52:12 +0200

    dashcastx: res as a param
    Former-commit-id: df11abcb836858ed42d23c691a2a5c44385dbe04

--- src/apps/dashcastx/options.hpp
+++ src/apps/dashcastx/options.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "lib_media/common/picture.hpp"
+
 struct dashcastXOptions {
 	std::string url;
+	Modules::Resolution res = Modules::VIDEO_RESOLUTION;
 	uint64_t segmentDuration = 2000;
 	bool isLive = false;
 };
