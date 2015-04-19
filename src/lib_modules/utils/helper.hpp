e652bbd37589a3dbacce2c01848b37a22da31231
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2015-04-19 17:47:17 +0200

    remove unused
    Former-commit-id: 35722a1580aa99e8b443eda42fe36bbdac1279ab

--- src/lib_modules/utils/helper.hpp
+++ src/lib_modules/utils/helper.hpp
@@ -14,44 +14,22 @@ MEMBER_FUNCTOR_PROCESS(Class* objectPtr) {
 	return Signals::MemberFunctor<void, Class, void(Class::*)(Data)>(objectPtr, &IProcessor::process);
 }
 
-//Romain: remove ConnectOutput?
-inline size_t ConnectOutput(IOutput* p, std::function<void(Data)> functor) {
-	return p->getSignal().connect(functor);
-}
-
-template<typename C, typename D>
-size_t ConnectOutput(IOutput* p, C ObjectSlot, D MemberFunctionSlot) {
-	auto functor = MEMBER_FUNCTOR(ObjectSlot, MemberFunctionSlot);
-	return ConnectOutput(p, functor);
-}
-
-inline size_t ConnectOutput(IOutput* p, std::function<void(Data)> functor, IProcessExecutor& executor) {
-	return p->getSignal().connect(functor, executor);
-}
-
-template<typename C, typename D, typename E>
-size_t ConnectOutput(IOutput* p, C ObjectSlot, D MemberFunctionSlot, E& executor) {
-	auto functor = MEMBER_FUNCTOR(ObjectSlot, MemberFunctionSlot);
-	return ConnectOutput(p, functor, executor);
-}
-
 template<typename ModuleType>
-size_t ConnectOutputToInput(IOutput* out, ModuleType* module, IProcessExecutor& executor = defaultExecutor) {
+size_t ConnectOutputToInput(IOutput* output, ModuleType* module, IProcessExecutor& executor = defaultExecutor) {
 	auto functor = MEMBER_FUNCTOR_PROCESS(module);
 	//Romain: we can check based on metadata media-type
-	return ConnectOutput(out, functor, executor);
+	return output->getSignal().connect(functor, executor);
 }
 
 template<typename ModuleType>
-size_t ConnectOutputToInput(IOutput* out, std::unique_ptr<ModuleType>& module, IProcessExecutor& executor = defaultExecutor) {
-	return ConnectOutputToInput(out, module.get(), executor);
+size_t ConnectOutputToInput(IOutput* output, std::unique_ptr<ModuleType>& module, IProcessExecutor& executor = defaultExecutor) {
+	return ConnectOutputToInput(output, module.get(), executor);
 }
 
 template<typename OutputType, typename ModuleType>
 size_t ConnectModules(OutputType *output, ModuleType *module, size_t inputIdx, IProcessExecutor& executor = defaultExecutor) {
 	auto input = module->getInput(inputIdx);
-	auto functor = MEMBER_FUNCTOR_PROCESS(input);
-	return ConnectOutput(output, functor, executor);
+	return ConnectOutputToInput(output, input, executor);
 }
 
 template<typename ModuleType1, typename ModuleType2>
