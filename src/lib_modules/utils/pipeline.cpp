9913e2a15e3f6689b1fed4fe5c77e81c5070b2ca
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2015-04-19 17:29:00 +0200

    move code to cpp
    Former-commit-id: ea24fda9a510e8d3242f51f73a2ea9d4f6107024

--- src/lib_modules/utils/pipeline.cpp
+++ src/lib_modules/utils/pipeline.cpp
@@ -3,6 +3,59 @@
 
 namespace Modules {
 
+PipelinedModule::PipelinedModule(Module *module, ICompletionNotifier *notify)
+: type(None), delegate(module), localExecutor(new EXECUTOR), executor(*localExecutor), m_notify(notify) {
+}
+
+size_t PipelinedModule::getNumInputs() const {
+	return delegate->getNumInputs();
+}
+IInput* PipelinedModule::getInput(size_t i) {
+	return delegate->getInput(i);
+}
+size_t PipelinedModule::getNumOutputs() const {
+	return delegate->getNumOutputs();
+}
+IOutput* PipelinedModule::getOutput(size_t i) const {
+	return delegate->getOutput(i);
+}
+
+void PipelinedModule::process(Data data) {
+	if (data) {
+		delegate->process(data);
+	} else {
+		endOfStream();
+	}
+}
+
+void PipelinedModule::dispatch(Data data) {
+	if (isSource()) {
+		assert(data == nullptr);
+		executor(MEMBER_FUNCTOR_PROCESS(delegate.get()), data);
+	}
+	executor(MEMBER_FUNCTOR_PROCESS(this), data);
+}
+
+void PipelinedModule::setSource(bool isSource) {
+	type = isSource ? Source : None;
+}
+bool PipelinedModule::isSource() const {
+	return type == Source;
+}
+bool PipelinedModule::isSink() const {
+	return delegate->getNumOutputs() == 0;
+}
+
+void PipelinedModule::endOfStream() {
+	delegate->flush();
+	if (isSink()) {
+		m_notify->finished();
+	} else {
+		for (size_t i = 0; i < delegate->getNumOutputs(); ++i)
+			delegate->getOutput(i)->emit(Data(nullptr));
+	}
+}
+
 Pipeline::Pipeline(bool isLowLatency) : isLowLatency(isLowLatency), numRemainingNotifications(0) {
 }
 
--- src/lib_modules/utils/pipeline.hpp
+++ src/lib_modules/utils/pipeline.hpp
@@ -31,9 +31,7 @@ struct IPipelinedModule : public IInputCap, public IOutputCap {
 class PipelinedModule : public IPipelinedModule {
 public:
 	/* take ownership of module */
-	PipelinedModule(Module *module, ICompletionNotifier *notify)
-	: type(None), delegate(module), localExecutor(new EXECUTOR), executor(*localExecutor), m_notify(notify) {
-	}
+	PipelinedModule(Module *module, ICompletionNotifier *notify);
 	~PipelinedModule () noexcept(false) {}
 
 	template<typename OutputType>
@@ -41,58 +39,24 @@ public:
 		ConnectModules(output, this, inputIdx, executor);
 	}
 
-	size_t getNumInputs() const override {
-		return delegate->getNumInputs();
-	}
-	IInput* getInput(size_t i) override {
-		return delegate->getInput(i);
-	}
-	size_t getNumOutputs() const override {
-		return delegate->getNumOutputs();
-	}
-	IOutput* getOutput(size_t i) const override {
-		return delegate->getOutput(i);
-	}
+	size_t getNumInputs() const override;
+	IInput* getInput(size_t i) override;
+	size_t getNumOutputs() const override;
+	IOutput* getOutput(size_t i) const override;
 
 	/* direct call: receiving nullptr stops the execution */
-	void process(Data data) {
-		if (data) {
-			delegate->process(data);
-		} else {
-			endOfStream();
-		}
-	}
+	void process(Data data);
 
 	/* same as process() but uses the executor (may defer the call) */
-	void dispatch(Data data) {
-		if (isSource()) {
-			assert(data == nullptr);
-			executor(MEMBER_FUNCTOR_PROCESS(delegate.get()), data);
-		}
-		executor(MEMBER_FUNCTOR_PROCESS(this), data);
-	}
+	void dispatch(Data data);
 
 	/* source modules are stopped manually - then the message propagates to other connected modules */
-	void setSource(bool isSource) {
-		type = isSource ? Source : None;
-	}
-	bool isSource() const {
-		return type == Source;
-	}
-	bool isSink() const {
-		return delegate->getNumOutputs() == 0;
-	}
+	void setSource(bool isSource);
+	bool isSource() const;
+	bool isSink() const;
 
 private:
-	void endOfStream() {
-		delegate->flush();
-		if (isSink()) {
-			m_notify->finished();
-		} else {
-			for (size_t i = 0; i < delegate->getNumOutputs(); ++i)
-				delegate->getOutput(i)->emit(Data(nullptr));
-		}
-	}
+	void endOfStream();
 
 	enum Type {
 		None,
