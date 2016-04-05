4615d702efbacebb1614e81cbf79b34cdd523029
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2016-04-05 20:59:23 +0200

    use ffmpeg 2.8.6 on all platforms
    Former-commit-id: a01d481ec59b87af6dd9c6cb35987ad5eb0e587c

--- extra.sh
+++ extra.sh
@@ -50,6 +50,7 @@ if [ ! -f extra/src/zenbuild/zenbuild.sh ] ; then
 	pushd extra/src/zenbuild
 	git checkout f63eac8a5bad
 	patch -p1 < ../../patches/gpac_01_revision.diff
+	patch -p1 < ../../patches/ffmpeg_01_version.diff
 	popd
 fi
 
