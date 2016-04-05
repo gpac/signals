57928de4d79fbc2d15558d7c95c2355324ada1f0
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2016-04-05 17:39:57 +0200

    allow dashcastx to be cleanly interrupted (close the windows, ctrl-c, etc.)
    Former-commit-id: 700183cadd1232afbf3977cdc39e49e0cbf28840

--- src/apps/dashcastx/dashcastx.cpp
+++ src/apps/dashcastx/dashcastx.cpp
@@ -1,19 +1,55 @@
 #include "lib_utils/profiler.hpp"
 #include "pipeliner.hpp"
+#include <csignal>
 
 using namespace Pipelines;
 
+Pipeline *g_Pipeline = nullptr;
+
+#ifdef _MSC_VER
+BOOL WINAPI signalHandler(_In_ DWORD dwCtrlType) {
+	switch (dwCtrlType) {
+	case CTRL_C_EVENT:
+	case CTRL_BREAK_EVENT:
+	case CTRL_CLOSE_EVENT:
+	case CTRL_LOGOFF_EVENT:
+	case CTRL_SHUTDOWN_EVENT:
+		printf("Exit event received.\n\n");
+		if (g_Pipeline) {
+			g_Pipeline->exitSync();
+		}
+		return TRUE;
+	default:
+		return FALSE;
+	}
+}
+#else
+void sigTermHandler(int sig) {
+	if (sig == SIGTERM) {
+		std::cerr << "Caught signal(" << dwCtrlType << "), exiting." << std::endl;
+	}
+}
+#endif
+
 int safeMain(int argc, char const* argv[]) {
+#ifdef _MSC_VER
+	SetConsoleCtrlHandler(signalHandler, TRUE);
+#else
+	std::signal(SIGTERM, sigTermHandler);
+#endif
+
 	dashcastXOptions opt = processArgs(argc, argv);
 
 	Tools::Profiler profilerGlobal("DashcastX");
 
-	Pipeline pipeline(opt.isLive);
-	declarePipeline(pipeline, opt);
+	auto pipeline = uptr(new Pipeline(opt.isLive));
+	declarePipeline(*pipeline, opt);
+	g_Pipeline = pipeline.get();
 
 	Tools::Profiler profilerProcessing("DashcastX - processing time");
-	pipeline.start();
-	pipeline.waitForCompletion();
+	std::cerr << "DashcastX - close window or ctrl-c to exit cleanly." << std::endl;
+	pipeline->start();
+	pipeline->waitForCompletion();
 
 	return 0;
 }
