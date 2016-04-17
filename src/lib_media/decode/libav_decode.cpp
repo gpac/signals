095de2cb3529a7f09e97b0efec4a6e3d5e3ca50c
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2016-04-17 18:49:49 +0200

    don't rely on ffmpeg returned size to compute audio sample number - #45

--- src/lib_media/decode/libav_decode.cpp
+++ src/lib_media/decode/libav_decode.cpp
@@ -78,7 +78,7 @@ bool LibavDecode::processAudio(const DataAVPacket *data) {
 		libavFrame2pcmConvert(avFrame->get(), &pcmFormat);
 		out->setFormat(pcmFormat);
 		for (uint8_t i = 0; i < pcmFormat.numPlanes; ++i) {
-			out->setPlane(i, avFrame->get()->data[i], avFrame->get()->linesize[0] / pcmFormat.numPlanes);
+			out->setPlane(i, avFrame->get()->data[i], avFrame->get()->nb_samples * pcmFormat.getBytesPerSample());
 		}
 		out->setTime(data->getTime());
 		audioOutput->emit(out);
