adb6f3a0893bae4958ec6df11764f7400313b6ea
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2016-04-18 17:22:40 +0200

    fix multiple resolution encoding - fix #46

--- src/lib_media/stream/mpeg_dash.cpp
+++ src/lib_media/stream/mpeg_dash.cpp
@@ -13,6 +13,24 @@
 #define AVAILABILITY_TIMEOFFSET_IN_S 0.0
 
 namespace Modules {
+
+namespace {
+GF_MPD_AdaptationSet *createAS(uint64_t segDurationInMs, GF_MPD_Period *period, gpacpp::MPD *mpd) {
+	auto as = mpd->addAdaptationSet(period);
+	GF_SAFEALLOC(as->segment_template, GF_MPD_SegmentTemplate);
+	as->segment_template->duration = segDurationInMs;
+	as->segment_template->timescale = 1000;
+	as->segment_template->start_number = 1;
+	as->segment_template->availability_time_offset = AVAILABILITY_TIMEOFFSET_IN_S;
+
+	//FIXME: arbitrary: should be set by the app, or computed
+	as->segment_alignment = GF_TRUE;
+	as->bitstream_switching = GF_TRUE;
+
+	return as;
+}
+}
+
 namespace Stream {
 
 MPEG_DASH::MPEG_DASH(const std::string &mpdPath, Type type, uint64_t segDurationInMs)
@@ -94,22 +112,21 @@ void  MPEG_DASH::ensureMPD() {
 
 		auto period = mpd->addPeriod();
 		period->ID = gf_strdup("p0");
+		GF_MPD_AdaptationSet *audioAS = nullptr, *videoAS = nullptr;
 		for (size_t i = 0; i < getNumInputs() - 1; ++i) {
-			auto as = mpd->addAdaptationSet(period);
-			GF_SAFEALLOC(as->segment_template, GF_MPD_SegmentTemplate);
-			as->segment_template->duration = segDurationInMs;
-			as->segment_template->timescale = 1000;
-			as->segment_template->media = gf_strdup(format("%s.mp4_$Number$", i).c_str());
-			as->segment_template->initialization = gf_strdup(format("$RepresentationID$.mp4", i).c_str());
-			as->segment_template->start_number = 1;
-			as->segment_template->availability_time_offset = AVAILABILITY_TIMEOFFSET_IN_S;
-
-			//FIXME: arbitrary: should be set by the app, or computed
-			as->segment_alignment = GF_TRUE;
-			as->bitstream_switching = GF_TRUE;
+			GF_MPD_AdaptationSet *as = nullptr;
+			switch (qualities[i].meta->getStreamType()) {
+			case AUDIO_PKT: audioAS ? as = audioAS : as = audioAS = createAS(segDurationInMs, period, mpd.get()); break;
+			case VIDEO_PKT: videoAS ? as = videoAS : as = videoAS = createAS(segDurationInMs, period, mpd.get()); break;
+			default: assert(0);
+			}
 
 			auto rep = mpd->addRepresentation(as, format("%s", i).c_str(), (u32)qualities[i].bitrate_in_bps);
 			qualities[i].rep = rep;
+			GF_SAFEALLOC(rep->segment_template, GF_MPD_SegmentTemplate);
+			rep->segment_template->media = gf_strdup(format("%s.mp4_$Number$", i).c_str());
+			rep->segment_template->initialization = gf_strdup(format("$RepresentationID$.mp4", i).c_str());
+			rep->segment_template->start_number = 1;
 			rep->mime_type = gf_strdup(qualities[i].meta->getMimeType().c_str());
 			rep->codecs = gf_strdup(qualities[i].meta->getCodecName().c_str());
 			rep->starts_with_sap = GF_TRUE;
