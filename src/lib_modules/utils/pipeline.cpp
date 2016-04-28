c1804d560216a0dc4bcd4dbe27b9c2b8c9efc572
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2016-04-28 08:34:05 +0200

    merge decl and def for pipeline only implementation

--- src/lib_modules/utils/pipeline.cpp
+++ src/lib_modules/utils/pipeline.cpp
@@ -12,19 +12,22 @@ using namespace Modules;
 
 namespace Pipelines {
 
+namespace {
 template<typename Class>
 Signals::MemberFunctor<void, Class, void(Class::*)()>
 MEMBER_FUNCTOR_NOTIFY_FINISHED(Class* objectPtr) {
 	return Signals::MemberFunctor<void, Class, void(Class::*)()>(objectPtr, &ICompletionNotifier::finished);
 }
+}
 
+/* Wrapper around the module's inputs. Data is queued in the calling thread, then always dispatched by the executor. */
 class PipelinedInput : public IInput {
 	public:
-		PipelinedInput(IInput *input, Modules::IProcessExecutor &executor, ICompletionNotifier * const notify)
+		PipelinedInput(IInput *input, IProcessExecutor &executor, ICompletionNotifier * const notify)
 			: delegate(input), notify(notify), executor(executor) {}
 		virtual ~PipelinedInput() noexcept(false) {}
 
-		/* direct call: receiving nullptr stops the execution */
+		/* receiving nullptr stops the execution */
 		virtual void process() override {
 			auto data = pop();
 			if (data) {
@@ -47,128 +50,108 @@ class PipelinedInput : public IInput {
 	private:
 		IInput *delegate;
 		ICompletionNotifier * const notify;
-		Modules::IProcessExecutor &executor;
+		IProcessExecutor &executor;
 };
 
-class PipelinedModule : public ICompletionNotifier, public IPipelinedModule, public Modules::InputCap {
+/* wrapper around the module */
+class PipelinedModule : public ICompletionNotifier, public IPipelinedModule, public InputCap {
 public:
 	/* take ownership of module */
-	PipelinedModule(Modules::IModule *module, ICompletionNotifier *notify);
+	PipelinedModule(IModule *module, ICompletionNotifier *notify)
+		: delegate(module), localExecutor(new EXECUTOR), executor(*localExecutor), m_notify(notify) {
+	}
 	~PipelinedModule() noexcept(false) {}
 
-	size_t getNumInputs() const override;
-	size_t getNumOutputs() const override;
-	Modules::IOutput* getOutput(size_t i) const override;
+	size_t getNumInputs() const override {
+		return delegate->getNumInputs();
+	}
+	size_t getNumOutputs() const override {
+		return delegate->getNumOutputs();
+	}
+	IOutput* getOutput(size_t i) const override {
+		if (i >= delegate->getNumOutputs())
+			throw std::runtime_error(format("PipelinedModule %s: no output %s.", typeid(delegate).name(), i));
+		return delegate->getOutput(i);
+	}
 
 	/* source modules are stopped manually - then the message propagates to other connected modules */
-	bool isSource() const override;
-	bool isSink() const override;
+	bool isSource() const override {
+		if (delegate->getNumInputs() == 0) {
+			return true;
+		} else if (delegate->getNumInputs() == 1 && dynamic_cast<Input<DataLoose, IProcessor>*>(delegate->getInput(0))) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	bool isSink() const override {
+		return delegate->getNumOutputs() == 0;
+	}
 
 private:
-	virtual void connect(Modules::IOutput *output, size_t inputIdx) override;
-	Modules::IInput* getInput(size_t i) override;
-	void mimicInputs();
+	virtual void connect(IOutput *output, size_t inputIdx) override {
+		ConnectOutputToInput(output, getInput(inputIdx), &g_executorSync);
+	}
+
+	void mimicInputs() {
+		auto const delegateInputs = delegate->getNumInputs();
+		auto const thisInputs = inputs.size();
+		if (thisInputs < delegateInputs) {
+			for (size_t i = thisInputs; i < delegateInputs; ++i) {
+				addInput(new PipelinedInput(delegate->getInput(i), this->executor, this));
+			}
+		}
+	}
+	IInput* PipelinedModule::getInput(size_t i) override {
+		mimicInputs();
+		if (i >= inputs.size())
+			throw std::runtime_error(format("PipelinedModule %s: no input %s.", typeid(delegate).name(), i));
+		return inputs[i].get();
+	}
 
 	/* uses the executor (i.e. may defer the call) */
-	void process() override;
-	void finished() override;
+	void process() override {
+		Log::msg(Debug, format("Module %s: dispatch data", typeid(delegate).name()));
 
-	std::unique_ptr<Modules::IModule> delegate;
-	std::unique_ptr<Modules::IProcessExecutor> const localExecutor;
-	Modules::IProcessExecutor &executor;
+		if (isSource()) {
+			if (getNumInputs() == 0) {
+				/*first time: create a fake pin and push null to trigger execution*/
+				delegate->addInput(new Input<DataLoose>(delegate.get()));
+				getInput(0)->push(nullptr);
+				delegate->getInput(0)->push(nullptr);
+				executor(MEMBER_FUNCTOR_PROCESS(delegate.get()));
+				executor(MEMBER_FUNCTOR_PROCESS(getInput(0)));
+				return;
+			} else {
+				/*the source is likely processing: push null in the loop to exit and let things follow their way*/
+				delegate->getInput(0)->push(nullptr);
+				return;
+			}
+		}
+
+		Data data = getInput(0)->pop();
+		for (size_t i = 0; i < getNumInputs(); ++i) {
+			getInput(i)->push(data);
+			getInput(i)->process();
+		}
+	}
+
+	void finished() override {
+		delegate->flush();
+		if (isSink()) {
+			m_notify->finished();
+		} else {
+			for (size_t i = 0; i < delegate->getNumOutputs(); ++i) {
+				delegate->getOutput(i)->emit(nullptr);
+			}
+		}
+	}
+
+	std::unique_ptr<IModule> delegate;
+	std::unique_ptr<IProcessExecutor> const localExecutor;
+	IProcessExecutor &executor;
 	ICompletionNotifier* const m_notify;
 };
-
-PipelinedModule::PipelinedModule(IModule *module, ICompletionNotifier *notify)
-	: delegate(module), localExecutor(new EXECUTOR), executor(*localExecutor), m_notify(notify) {
-}
-
-void PipelinedModule::mimicInputs() {
-	auto const delegateInputs = delegate->getNumInputs();
-	auto const thisInputs = inputs.size();
-	if (thisInputs < delegateInputs) {
-		for (size_t i = thisInputs; i < delegateInputs; ++i) {
-			addInput(new PipelinedInput(delegate->getInput(i), this->executor, this));
-		}
-	}
-}
-
-size_t PipelinedModule::getNumInputs() const {
-	return delegate->getNumInputs();
-}
-
-IInput* PipelinedModule::getInput(size_t i) {
-	mimicInputs();
-	if (i >= inputs.size())
-		throw std::runtime_error(format("PipelinedModule %s: no input %s.", typeid(delegate).name(), i));
-	return inputs[i].get();
-}
-
-size_t PipelinedModule::getNumOutputs() const {
-	return delegate->getNumOutputs();
-}
-
-IOutput* PipelinedModule::getOutput(size_t i) const {
-	if (i >= delegate->getNumOutputs())
-		throw std::runtime_error(format("PipelinedModule %s: no output %s.", typeid(delegate).name(), i));
-	return delegate->getOutput(i);
-}
-
-bool PipelinedModule::isSource() const {
-	if (delegate->getNumInputs() == 0) {
-		return true;
-	} else if (delegate->getNumInputs() == 1 && dynamic_cast<Modules::Input<DataLoose, Modules::IProcessor>*>(delegate->getInput(0))) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-bool PipelinedModule::isSink() const {
-	return delegate->getNumOutputs() == 0;
-}
-
-void PipelinedModule::connect(IOutput *output, size_t inputIdx) {
-	ConnectOutputToInput(output, getInput(inputIdx), &g_executorSync);
-}
-
-void PipelinedModule::process() {
-	Log::msg(Debug, format("Module %s: dispatch data", typeid(delegate).name()));
-
-	if (isSource()) {
-		if (getNumInputs() == 0) {
-			/*first time: create a fake pin and push null to trigger execution*/
-			delegate->addInput(new Input<DataLoose>(delegate.get()));
-			getInput(0)->push(nullptr);
-			delegate->getInput(0)->push(nullptr);
-			executor(MEMBER_FUNCTOR_PROCESS(delegate.get()));
-			executor(MEMBER_FUNCTOR_PROCESS(getInput(0)));
-			return;
-		} else {
-			/*the source is likely processing: push null in the loop to exit and let things follow their way*/
-			delegate->getInput(0)->push(nullptr);
-			return;
-		}
-	}
-
-	Data data = getInput(0)->pop();
-	for (size_t i = 0; i < getNumInputs(); ++i) {
-		getInput(i)->push(data);
-		getInput(i)->process();
-	}
-}
-
-/* end of stream */
-void PipelinedModule::finished() {
-	delegate->flush();
-	if (isSink()) {
-		m_notify->finished();
-	} else {
-		for (size_t i = 0; i < delegate->getNumOutputs(); ++i) {
-			delegate->getOutput(i)->emit(nullptr);
-		}
-	}
-}
 
 Pipeline::Pipeline(bool isLowLatency) : isLowLatency(isLowLatency), numRemainingNotifications(0) {
 }
